70fe465bc8888df37d34c5632ebed23067dfa3a7
sagun basnet <basnetsagun8@gmail.com>
2025-07-11 13:28:10 +0545

    useEffec

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -1,31 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Count = () => {
   const [count, setCount] = useState(0);
   const [isDisplay, setIsDisplay] = useState(false);
   //   let count = 0;
 
-  const add = (a, b) => {
+  useEffect(() => {
+    console.log("i am from useEffect");
+  }, []);
+
+  const add = () => {
     // count += 1;
     setCount((prev) => prev + 1);
     setCount((prev) => prev + 1);
     console.log(count);
   };
 
+  const handleDisplay = () => {
+    if (isDisplay === true) {
+      setIsDisplay(false);
+    } else {
+      setIsDisplay(true);
+    }
+  };
+
   return (
     <div className="flex flex-col justify-center items-center">
       <span>{count}</span>
-      <button
-        className="p-2 px-4 bg-green-600 rounded-md"
-        onClick={() => add(12, 34)}
-      >
+      <button className="p-2 px-4 bg-green-600 rounded-md" onClick={add}>
         Add
       </button>
       <button
-        onClick={() => setIsDisplay(true)}
+        onClick={handleDisplay}
         className="p-2 px-4 bg-green-600 rounded-md"
       >
-        see more
+        {isDisplay ? "see less" : "see more"}
       </button>
       {isDisplay && (
         <p>
